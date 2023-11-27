3050806f46b7c8f94f816c52c0fd70361107de31
MrMelon54 <github@mrmelon54.com>
2023-11-27 00:40:15 +0000

    Fix postfix lookup.sh

--- postfix-lookup/lookup.sh
+++ postfix-lookup/lookup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-virtual_alias_maps=$(postconf -h virtual_alias_maps | tr ',' '\n')
+virtual_alias_maps=$(postconf -h smtpd_sender_login_maps)
 alias_to_lookup="$1"
 result=$(echo "$virtual_alias_maps" | xargs -I {} postmap -q "$alias_to_lookup" {})
 echo "result=$result"
